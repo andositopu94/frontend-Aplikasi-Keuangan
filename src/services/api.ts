e3a488ca58230f41a532d31ded5f8de009f6bef5
Forando Hotman <andositopu@gmail..com>
2026-04-17 23:14:14 +0700

    setup environtment

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,43 +2,42 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 import { error } from "console";
 import { config } from "process";
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://103.197.190.226:8585/api";
+
+console.log("API Base URL:", API_BASE_URL);
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    // baseURL: "http://localhost:8080/api",
+  baseURL: API_BASE_URL,
     headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
-  }
+  }, 
+  withCredentials: true
 });
 
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('authToken');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
-// apiClient.interceptors.request.use(
-//   (config: InternalAxiosRequestConfig) => { 
-//     const token = localStorage.getItem('authToken');
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
 
 apiClient.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    return response;
+  },
   (error) => {
     if (error.response?.status === 401 || error.response?.status === 403) {
       localStorage.removeItem('authToken');
       localStorage.removeItem('userRole');
-      window.location.href = '/login';
+      
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
     }
     return Promise.reject(error);
   }
